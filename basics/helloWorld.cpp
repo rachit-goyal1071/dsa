6c6a7f6978814fbb98a5345b3e6c0a2cf58dfaa1
Rachit Goyal <rg410345@gmail.com>
2024-02-14 23:32:43 +0530

    Started dsa learning c++ basics Refrence: https://www.geeksforgeeks.org/c-plus-plus

---
+++ basics/helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+// std contains all the necessary library can also use using namespace std 
+// using namespace std;
+    std::cout<<"Hello World";
+    
+    return 0;
+}
---
+++ variableDeclaration.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a ,b=5;
+    int c = 10;
+    int d = c+b;
+    cout<<a <<endl <<endl <<b <<endl <<endl <<c <<"     "<< d;
+}
